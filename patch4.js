dca61e803679f1cff2bf3020a472f3ac2b8a83c6
Bruno Vieira <bv721706@gmail.com>
2026-08-31 23:33:03 +0100

    refactor: remove unused components and clean up function signatures in StreetLayout and DeviceListPanel

--- patch4.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('frontend/src/scene/StreetLayout.tsx', 'utf8');
-
-code = code.replace(
-  "export function StreetLayout({ scanData, offsetX, offsetZ, gridWidth, gridDepth }: { scanData: any, offsetX: number, offsetZ: number, gridWidth: number, gridDepth: number }) {",
-  "export function StreetLayout({ offsetX, offsetZ, gridWidth, gridDepth }: { offsetX: number, offsetZ: number, gridWidth: number, gridDepth: number }) {"
-);
-
-fs.writeFileSync('frontend/src/scene/StreetLayout.tsx', code);
